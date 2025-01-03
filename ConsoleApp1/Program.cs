7e69f6bdba8a3f4fb3783f96d090fa8431604f41
Rares Molnar <raress.molnar@gmail.com>
2025-01-03 21:06:11 +0200

    a si b

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,5 +7,11 @@ class Program
         Console.WriteLine("Modificare adusa de Rares!!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
+
+        int a = 10;
+        int b = 2;
+        int result = a + b;
+        Console.WriteLine(result);
+            
     }
 }
