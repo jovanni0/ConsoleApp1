505b67603c9f8ff0ed39a9d9625c9d1519b27a46
Rares Molnar <raress.molnar@gmail.com>
2025-01-03 20:58:55 +0200

    modificare test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Modificare adusa de Rares!!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
     }
